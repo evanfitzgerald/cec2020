9fd69bbd34442c6ef35b6ef5d82966ec3b9dfd6b
Mackenzie Toner <mackenzie.toner@unb.ca>
2020-03-07 16:47:58 -0400

    Clean up

--- level3/src/App.vue
+++ level3/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <p>CEC 2020 - Prorgramming</p>
-    <span>
-      <p>Rotate model</p>
-      <input type="checkbox" id="horizontal" v-model="horizontal">
-      <label for="horizontal">Horizontal</label>
-      <input type="checkbox" id="vertical" v-model="vertical">
-      <label for="vertical">Vertical</label>
-    </span>
+      <p>
+        <label for="filename">File Name: </label>
+        <input id="filename" v-model="filename" :placeholder="filename">
+        <button @click="getData">Load data</button>
+      </p>
+      <p>Rotate model: 
+        <input type="checkbox" id="horizontal" v-model="horizontal">
+        <label for="horizontal">Horizontal</label>
+        <input type="checkbox" id="vertical" v-model="vertical">
+        <label for="vertical">Vertical</label>
+      </p>
     <Three ref="threecomponent" :size="size" :inputGrid="inputGrid" :vertical="vertical" :horizontal="horizontal"></Three>
   </div>
 </template>
@@ -27,18 +31,30 @@ export default {
       vertical: false,
       size: null,
       inputGrid: null,
+      filename: 'easy.txt'
     }
   },
   mounted() {
-    axios
-      .get("http://localhost:5000/getgrid/easy.txt")
-      .then(response => (this.handleResponse(response)));
+    this.getData();
   },
   methods: {
     handleResponse(response) {
       this.size = response.data.size;
       this.inputGrid = response.data.grid;
       this.$refs.threecomponent.updateGrid(this.size, this.inputGrid);
+    },
+    handleError(error) {
+      if (!error.response) {
+        alert('Network error: Could not connect to server');
+      } else {
+        alert("'" + this.filename + "'\nFile does not exist");
+      }
+    },
+    getData() {
+      axios
+        .get("http://localhost:5000/getgrid/" + this.filename)
+        .then(response => (this.handleResponse(response)))
+        .catch(error => (this.handleError(error)));
     }
   },
 }
