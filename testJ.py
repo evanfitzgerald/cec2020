2b4bdbedd61c7dc9268e1ba47a32dfc96589cdf3
Evan Fitzgerald <evandfitz@gmail.com>
2020-03-07 15:08:44 -0400

    adding test file

--- testJ.py
+++ testJ.py
@@ -5,21 +5,44 @@ from readfile import readfile
 import math
 
 cube = readfile("easy.txt")
-size = 5**3
+sizec = 5**3
+size = 5
 mode = 1
-storHop = int(math.sqrt(size)/2)
+storHop = int(math.sqrt(sizec)/2)
 drone = Drone(storHop,cube,0,0,size)
 print(cube)
 drone.hop()
+x_limit = size-1
+y_limit = size-1
 
-if (mode == 1):
-    drone.right()
-    if (x == x_limit)
-        mode = 3
-elif (mode == 2):
-    drone.left()
-elif (mode == 3):
-    drone.down()
-else:
-    dronw.up()
+
+def mover(mode, x_limit, y_limit):
+    if (mode == 1):
+        drone.MoveRight()
+        if (drone.x == x_limit):
+            mode = 3
+    elif (mode == 2):
+        drone.MoveLeft()
+        if (drone.x == size - (x_limit+1)):
+            mode = -1
+    elif (mode == 3):
+        drone.MoveDown()
+        if (drone.y == y_limit):
+            mode = 2
+    else:
+        drone.MoveUp()
+        if (drone.y == size - y_limit):
+            mode = 1
+            y_limit = y_limit - 1
+            x_limit = x_limit - 1
+
+    return mode, x_limit, y_limit
+
+
+for i in range(24):
+    mode, x_limit, y_limit = mover(mode, x_limit, y_limit)
+    print(drone.x,drone.y)
+drone.hop()
+
+
 print(cube)
